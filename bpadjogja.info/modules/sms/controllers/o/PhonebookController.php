974aeffca6bcecb9378bcc984b402c2c8ebd1ae6
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-02-25 00:47:47 +0700

    sms: controller add user level admin and moderator condition

--- bpadjogja.info/modules/sms/controllers/o/PhonebookController.php
+++ bpadjogja.info/modules/sms/controllers/o/PhonebookController.php
@@ -46,7 +46,7 @@ class PhonebookController extends Controller
 	public function init() 
 	{
 		if(!Yii::app()->user->isGuest) {
-			if(Yii::app()->user->level == 1) {
+			if(in_array(Yii::app()->user->level, array(1,2))) {
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
@@ -90,7 +90,7 @@ class PhonebookController extends Controller
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('manage','suggest','import','add','edit','view','runaction','delete','status'),
 				'users'=>array('@'),
-				'expression'=>'isset(Yii::app()->user->level) && (Yii::app()->user->level == 1)',
+				'expression'=>'isset(Yii::app()->user->level) && in_array(Yii::app()->user->level, array(1,2))',
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array(),
