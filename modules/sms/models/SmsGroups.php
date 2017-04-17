c80c06c6faefefdea13965826a82fdaf8df36df0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-17 21:36:21 +0700

    add reinstalldatepicker function in gridview filter

--- modules/sms/models/SmsGroups.php
+++ modules/sms/models/SmsGroups.php
@@ -245,7 +245,7 @@ class SmsGroups extends CActiveRecord
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
+				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'creation_date',
 					'language' => 'en',
--- modules/sms/models/SmsOutboxGroup.php
+++ modules/sms/models/SmsOutboxGroup.php
@@ -200,7 +200,7 @@ class SmsOutboxGroup extends CActiveRecord
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
+				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'creation_date',
 					'language' => 'en',
