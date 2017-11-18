61df34bdd15d128b863e45d01d11d2bba19e641f
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-19 05:15:01 +0700

    commit

---
+++ modules/sms/views/o/outbox/admin_delete.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Sms Outboxes (sms-outbox)
+ * @var $this OutboxController
+ * @var $model SmsOutbox
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co)
+ * @created date 12 February 2016, 04:07 WIB
+ * @link https://github.com/ommu/mod-sms
+ * @contact (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Sms Inboxes'=>array('manage'),
+		'Delete',
+	);
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'sms-outbox-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+	<div class="dialog-content">
+		<?php echo Yii::t('phrase', 'Are you sure you want to delete this item?');?>
+	</div>
+	<div class="dialog-submit">
+		<?php echo CHtml::submitButton(Yii::t('phrase', 'Delete'), array('onclick' => 'setEnableSave()')); ?>
+		<?php echo CHtml::button(Yii::t('phrase', 'Cancel'), array('id'=>'closed')); ?>
+	</div>
+	
+<?php $this->endWidget(); ?>
